0e049339619d7987973cc83e825f70192bc1aa25
Chudnovskyi <chudnovskiy0@gmail.com>
2023-04-18 20:24:33 +0200

    [Test] add test cases for LogoutService

--- authentication-service/src/main/java/com/example/authentication/config/SecurityConfig.java
+++ authentication-service/src/main/java/com/example/authentication/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.authentication.config;
 
+import com.example.authentication.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
