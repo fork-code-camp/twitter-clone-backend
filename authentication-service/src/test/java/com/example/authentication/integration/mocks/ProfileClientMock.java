a7d8984437b76fd53126e67a89b51293b26795ba
Eann1S <danilio.ponomarev7766@gmail.com>
2023-06-18 17:26:25 +0400

    [Feature] add readme for project [Bugfix] fix bugs in tests [Refactor] update docker compose

--- authentication-service/src/test/java/com/example/authentication/integration/mocks/ProfileClientMock.java
+++ authentication-service/src/test/java/com/example/authentication/integration/mocks/ProfileClientMock.java
@@ -2,16 +2,14 @@ package com.example.authentication.integration.mocks;
 
 import com.example.authentication.client.ProfileServiceClient;
 import com.example.authentication.client.request.CreateProfileRequest;
-import org.mockito.Mockito;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 public class ProfileClientMock {
 
-    public static void setupMockProfileResponse(ProfileServiceClient mockService) {
-        Mockito
-                .doReturn("dummy-profile-id")
-                .when(mockService)
-                .createProfile(any(CreateProfileRequest.class));
+    public static void setupMockProfileResponse(ProfileServiceClient profileServiceClient) {
+        when(profileServiceClient.createProfile(any(CreateProfileRequest.class)))
+                .thenReturn("dummy-profile-id");
     }
 }
--- tweet-service/src/test/java/com/example/tweet/integration/IntegrationTestBase.java
+++ tweet-service/src/test/java/com/example/tweet/integration/IntegrationTestBase.java
@@ -20,7 +20,9 @@ public class IntegrationTestBase {
 
     private static final PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:alpine3.17"));
     private static final GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:7.2-rc-alpine3.18")).withExposedPorts(6379);
-    private static final GenericContainer<?> configServerContainer = new FixedHostPortGenericContainer<>("twitterclone0/spring-cloud-config-server")
+    private static final GenericContainer<?> configServerContainer = new FixedHostPortGenericContainer<>(
+            "twitterclone0/twitter-spring-cloud-config-server:2.0.0"
+    )
             .withFixedExposedPort(8888, 8888)
             .waitingFor(Wait.forHttp("/tweet-service/test")
                     .forStatusCodeMatching(port -> port >= 200 && port < 400));
