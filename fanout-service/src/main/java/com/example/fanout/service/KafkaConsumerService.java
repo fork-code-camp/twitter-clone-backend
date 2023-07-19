3831311118d3206823ec74ca324848f1cd163568
Eann1S <danilio.ponomarev7766@gmail.com>
2023-07-19 22:25:38 +0400

    [Feature] implement asynchronous processing for messages from kafka

--- fanout-service/src/main/java/com/example/fanout/service/KafkaConsumerService.java
+++ fanout-service/src/main/java/com/example/fanout/service/KafkaConsumerService.java
@@ -5,7 +5,10 @@ import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
 
 import static com.example.fanout.constants.TopicName.HOME_TIMELINE_TOPIC;
 import static com.example.fanout.constants.TopicName.USER_TIMELINE_TOPIC;
@@ -19,17 +22,21 @@ public class KafkaConsumerService {
     private final FanoutService fanoutService;
     private final Gson gson;
 
+    @Async
     @KafkaListener(topics = USER_TIMELINE_TOPIC)
-    public void userTimelineListener(String msg) {
+    public CompletableFuture<Void> receiveEntityMessageForUserTimeline(String msg) {
         log.info("message for {} has been successfully received {}", USER_TIMELINE_TOPIC, msg);
         EntityMessage entityMessage = gson.fromJson(msg, EntityMessage.class);
         fanoutService.processMessageForUserTimeline(entityMessage);
+        return CompletableFuture.completedFuture(null);
     }
 
+    @Async
     @KafkaListener(topics = HOME_TIMELINE_TOPIC)
-    public void homeTimelineListener(String msg) {
+    public CompletableFuture<Void> receiveEntityMessageForHomeTimeline(String msg) {
         log.info("message for {} has been successfully received {}", HOME_TIMELINE_TOPIC, msg);
         EntityMessage entityMessage = gson.fromJson(msg, EntityMessage.class);
         fanoutService.processMessageForHomeTimeline(entityMessage);
+        return CompletableFuture.completedFuture(null);
     }
 }
